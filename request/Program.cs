54a8b3e76a8da18c740c485764a7bb3164eb8045
Percy <maldonadoq.percy@gmail.com>
2020-02-03 10:25:53 -0500

    C# get request in console project

---
+++ request/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace request
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL = "https://jsonplaceholder.typicode.com/posts";
+            var client = new WebClient();
+            var data = client.DownloadString(API_URL);
+
+            dynamic posts = JsonConvert.DeserializeObject(data);
+
+            foreach(var post in posts){
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+
+        }
+    }
+}
